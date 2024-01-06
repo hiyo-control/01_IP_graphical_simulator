22a2f37ccae0f81a957abea81deb03a69d7943ee
hiyo-control <hiyo.control@gmail.com>
2024-01-06 09:16:37 +0900

    file for arduino

---
+++ arduino_uart_acceleration/arduino_uart_acceleration.ino
@@ -0,0 +1,106 @@
+//================================================================//
+//  AE-BMX055             Arduino UNO                             //
+//    VCC                    +5V                                  //
+//    GND                    GND                                  //
+//    SDA                    A4(SDA)                              //
+//    SCL                    A5(SCL)                              //
+//                                                                //
+//   (JP4,JP5,JP6はショートした状態)                                //
+//   http://akizukidenshi.com/catalog/g/gK-13010/                 //
+//================================================================//
+
+#include <Wire.h>
+// BMX055 加速度センサのI2Cアドレス  
+#define Addr_Accl 0x19  // (JP1,JP2,JP3 = Openの時)
+// BMX055 ジャイロセンサのI2Cアドレス
+#define Addr_Gyro 0x69  // (JP1,JP2,JP3 = Openの時)
+// BMX055 磁気センサのI2Cアドレス
+#define Addr_Mag 0x13   // (JP1,JP2,JP3 = Openの時)
+
+// センサーの値を保存するグローバル変数
+unsigned int data[6];
+int Input_pin  = 3;
+int Output_pin = 5;
+
+void setup()
+{
+  // Wire(Arduino-I2C)の初期化
+  Wire.begin();
+  // デバッグ用シリアル通信は9600bps
+  Serial.begin(9600);
+  //BMX055 初期化
+  BMX055_Init();
+
+  pinMode(Input_pin , INPUT );
+  pinMode(Output_pin, OUTPUT);
+  
+  delay(300);
+}
+
+void loop()
+{
+  int switch_value = digitalRead(Input_pin);
+
+  //BMX055 加速度の読み取り
+  BMX055_Accl();
+
+  // Raspberry PI にデータ送信
+  Serial.write('H');
+  Serial.write( switch_value );
+  for (int i=0; i<6; i++)
+  {
+    Serial.write( data[i] );
+  }
+
+  // LED制御
+  if (switch_value == 1)
+  {
+    digitalWrite(Output_pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(Output_pin, LOW);
+  }
+  
+  delay(10);
+}
+
+//=====================================================================================//
+void BMX055_Init()
+{
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x0F); // Select PMU_Range register
+  Wire.write(0x03);   // Range = +/- 2g
+  Wire.endTransmission();
+  delay(100);
+ //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x10);  // Select PMU_BW register
+  Wire.write(0x08);  // Bandwidth = 7.81 Hz
+  Wire.endTransmission();
+  delay(100);
+  //------------------------------------------------------------//
+  Wire.beginTransmission(Addr_Accl);
+  Wire.write(0x11);  // Select PMU_LPW register
+  Wire.write(0x00);  // Normal mode, Sleep duration = 0.5ms
+  Wire.endTransmission();
+  delay(100);
+  
+}
+//=====================================================================================//
+void BMX055_Accl()
+{
+  //unsigned int data[6];
+  for (int i = 0; i < 6; i++)
+  {
+    Wire.beginTransmission(Addr_Accl);
+    Wire.write((2 + i));// Select data register
+    Wire.endTransmission();
+    Wire.requestFrom(Addr_Accl, 1);// Request 1 byte of data
+    // Read 6 bytes of data
+    // xAccl lsb, xAccl msb, yAccl lsb, yAccl msb, zAccl lsb, zAccl msb
+    if (Wire.available() == 1)
+      data[i] = Wire.read();
+  }
+}
